5a1e07406bcf0d0fa8f10466b6c1030cc7342fea
ProveY0uWorthy <100872517+ProveY0urWorth@users.noreply.github.com>
2022-12-26 21:06:54 +0300

    Updated Query

--- Schv3/Query/FrontQuery.cs
+++ Schv3/Query/FrontQuery.cs
@@ -4,13 +4,14 @@ using Schv3.Data;
 using Schv3.Interfaces;
 using Schv3.Models;
 using System.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Schv3.Query
 {
     public class FrontQuery
     {
-        public static FrontDataModel GetClassForFront (string date, string groupcode)
+        public static List<FrontDataModel> GetClassForFront (string date, string groupcode)
         {
             using (var db = new AppDataDbContext())
             {
@@ -30,62 +31,97 @@ namespace Schv3.Query
                            Classes.Id == query.FirstOrDefault().Id_8Class.Id 
                            select Classes;
                 var classesforday = query2.ToList();
+                int [] sch =
+                {
+                    query.FirstOrDefault().Id_1Class.Id,
+                    query.FirstOrDefault().Id_2Class.Id,
+                    query.FirstOrDefault().Id_3Class.Id,
+                    query.FirstOrDefault().Id_4Class.Id,
+                    query.FirstOrDefault().Id_5Class.Id,
+                    query.FirstOrDefault().Id_6Class.Id,
+                    query.FirstOrDefault().Id_7Class.Id,
+                    query.FirstOrDefault().Id_8Class.Id
+                };
 
-                
-                return OutVal;
-
-            }
-            
-        }
-
-        /*
-        var sch = query.FirstOrDefault();
-
-                var query3 = from ClassRooms in db.Classrooms
-                             where ClassRooms.Id == query2.FirstOrDefault().Id_Classroom
-                             select ClassRooms;
                 string para = "N/A";
-                if (query2.FirstOrDefault().Id == sch.Id_1Class.Id)
-                { para = "8:30 - 10:00"; } 
-                else if (query2.FirstOrDefault().Id == sch.Id_2Class.Id)
-                { para = "10:10 - 11:40"; }
-                else if (query2.FirstOrDefault().Id == sch.Id_3Class.Id)
-                { para = "1:50 - 13:20"; }
-                else if (query2.FirstOrDefault().Id == sch.Id_4Class.Id)
-                { para = "14:00 - 15:30"; }
-                else if (query2.FirstOrDefault().Id == sch.Id_5Class.Id)
-                { para = "15:40 - 17:10";  para = "8:30 - 10:10"; }
-                else if (query2.FirstOrDefault().Id == sch.Id_6Class.Id)
-                { para = "17:20 - 18:50"; }
-                else if (query2.FirstOrDefault().Id == sch.Id_7Class.Id)
-                { para = "19:00 - 20:30"; }
-                else if (query2.FirstOrDefault().Id == sch.Id_8Class.Id)
-                { para = "20:40 - 22:10"; }
-
-
+                var help = sch.ElementAt(0);
+                foreach(var lesson in classesforday)
+                {
+                    switch (lesson.Id)
+                    {
+                        case var value when value == sch[0]:
+                            para = "8:30 - 10:00";
+                            break;
+                        case var value when value == sch[1]:
+                            para = "10:10 - 11:40";
+                            break;
+                        case var value when value == sch[2]:
+                            para = "1:50 - 13:20";
+                            break;
+                        case var value when value == sch[3]:
+                            para = "14:00 - 15:30";
+                            break;
+                        case var value when value == sch[4]:
+                            para = "15:40 - 17:10";
+                            break;
+                        case var value when value == sch[5]:
+                            para = "17:20 - 18:50";
+                            break;
+                        case var value when value == sch[6]:
+                            para = "19:00 - 20:30";
+                            break;
+                        case var value when value == sch[7]:
+                            para = "20:40 - 22:10";
+                            break;
+                        default: break;
+                    }
+                }
                 // 1 лекция
                 // 2 практика
                 // 3 лз
 
-                string TypPary;
-                switch (query2.FirstOrDefault().ClassType)
+                string lessonType = "";
+                foreach (var item in classesforday)
                 {
-                    case 1: TypPary = "Л"; break;
-                    case 2: TypPary = "ПЗ"; break;
-                    case 3: TypPary = "ПЗ"; break;
-                    default: TypPary = "ПЗ"; break;
+                    switch (item.ClassType)
+                    {
+                            case 1: lessonType = "Л"; break;
+                            case 2: lessonType = "ПЗ"; break;
+                            case 3: lessonType = "ПЗ"; break;
+                            default: lessonType = "ПЗ"; break;
+                    }
                 }
 
-                 FrontDataModel OutVal = new FrontDataModel(
-                        query2.FirstOrDefault().Id,
-                        query2.FirstOrDefault().Id_Subject.Name,
-                        query2.FirstOrDefault().Group_Code.GroupCode,
-                        query2.FirstOrDefault().Id_Teacher.LName,
-                        query3.FirstOrDefault().Name,
-                        TypPary,
-                        query.FirstOrDefault().date,
-                        para
-                    );
-        */
+                List<FrontDataModel> ret = new List<FrontDataModel>();
+                for(var i = 0; i < classesforday.Count; i++)
+                {
+                    FrontDataModel st = new FrontDataModel(
+                       classesforday.ElementAt(i).Id,
+                       classesforday.ElementAt(i).Id_Subject.Name,
+                       classesforday.ElementAt(i).Group_Code.GroupCode,
+                       classesforday.ElementAt(i).Id_Teacher.LName,
+                       RoomName(classesforday.ElementAt(i)),
+                       lessonType,
+                       query.FirstOrDefault().date,
+                       para
+                   );
+                    ret.Add( st );
+                }
+                return ret;
+            }
+        }
+
+        public static string RoomName(Class el)
+        {
+
+            using (var db = new AppDataDbContext())
+            {
+                var query3 = from ClassRooms in db.Classrooms
+                             where ClassRooms.Id == el.Id_Classroom
+                             select ClassRooms.Name;
+                
+                return query3.FirstOrDefault();
+            }
+        }
     }
 }
